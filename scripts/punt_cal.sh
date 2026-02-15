ca169261bea0f3d6020bea17a55dc0988c801fe3
MysticChordsOfRAM <bitetheroundingchurros@proton.me>
2026-02-14 23:34:46 -0500

    ok now it all works

--- scripts/punt_cal.sh
+++ scripts/punt_cal.sh
@@ -13,17 +13,17 @@ SOURCE_FILE="/output/$CALENDAR_FILENAME"
 DESTINATION="$PUNT_DESTINATION"
 
 if [ -f "$SOURCE_FILE" ]; then
-    echo "[$(date + '%H:%M:%S')] Found $SOURCE_FILE. Starting transfer..."
+    echo "Found $SOURCE_FILE. Starting transfer..."
 
     rsync -avz "$SOURCE_FILE" "$DESTINATION"
 
     IF_SUCCESS=$?
-    if [$IF_SUCCESS -eq 0 ]; then
-        echo "[$(date + '%H:%M:%S')] Transfer to Lomez Successfull."
+    if [ $IF_SUCCESS -eq 0 ]; then
+        echo "Transfer to Lomez Successfull."
     else
-        echo "[$(date + '%H:%M:%S')] Error: Rsync Fail. Exit code $IF_SUCCESS"
+        echo "Error: Rsync Fail. Exit code $IF_SUCCESS"
         exit $IF_SUCCESS
     fi
 else
-    echo "[$(date + '%H:%M:%S')] Error Source File Not Found"
+    echo "Error Source File Not Found"
 fi
